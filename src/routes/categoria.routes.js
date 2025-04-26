1612b6dc433f40a087cd6ec2865031daafb0c907
josethgatica <josethgatica@gmail.com>
2025-04-25 22:46:53 -0600

    metodos de eliminar y actualizar en categorias

--- src/routes/categoria.routes.js
+++ src/routes/categoria.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {  obtenerCategoria, obtenerCategoriaid, registrarCategoria } from '../controllers/categoria.controller.js';
+import {  obtenerCategoria, obtenerCategoriaid, registrarCategoria, eliminarCategoria, actualizarCategoria } from '../controllers/categoria.controller.js';
 
 
 const router = Router();
@@ -13,6 +13,14 @@ router.get('/categorias/:id', obtenerCategoriaid);
 
 // ruta para registrar caregoria
 router.post('/registrarcategoria', registrarCategoria);
+
+// Ruta para eliminar un cliente por su ID
+router.delete('/eliminarcategoria/:id', eliminarCategoria);
+
+// Ruta para actualizar una categoría por su ID
+router.patch('/actualizarcategoria/:id', actualizarCategoria);
+
+
 
 
 export default router;
